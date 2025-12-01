17bc65694f000aa2135c55ea070b7b44be9835d9
rezi <c-revazi.karanadze@rithum.com>
2025-12-01 13:36:36 +0400

    implement auth pages

---
+++ apps/web/app/signin/page.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+export default function SigninPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-background py-12 px-4 sm:px-6 lg:px-6">
+      <div className="max-w-md w-full space-y-8">
+        <div className="text-center">
+          <h2 className="mt-6 text-3xl font-extrabold text-foreground">
+            Sign in to your account
+          </h2>
+          <p className="mt-2 text-sm text-muted-foreground font-extrabold">
+            Don&apos;t have an account?{" "}
+            <Link
+              href="/signup"
+              className="font-medium text-primary hover:text-primary/90"
+            >
+              Create an account
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ apps/web/app/signup/page.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+export default function SignupPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-background py-12 px-4 sm:px-6 lg:px-6">
+      <div className="max-w-md w-full space-y-8">
+        <div className="text-center">
+          <h2 className="mt-6 text-3xl font-extrabold text-foreground">
+            Create Your Account
+          </h2>
+          <p className="mt-2 text-sm text-muted-foreground font-extrabold">
+            Already have an account?{" "}
+            <Link
+              href="/signin"
+              className="font-medium text-primary hover:text-primary/90"
+            >
+              Sign in
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
