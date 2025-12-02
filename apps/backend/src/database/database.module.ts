d09bf60ca6bccf244bf0df9ef49dea5c099b6893
rezi <c-revazi.karanadze@rithum.com>
2025-12-02 15:17:25 +0400

    implement database connection

---
+++ apps/backend/src/database/database.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { DATABASE_CONNECTION } from './database-connection';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Pool } from 'pg';
+import { drizzle } from 'drizzle-orm/node-postgres';
+
+@Module({
+  imports: [ConfigModule],
+  providers: [
+    {
+      provide: DATABASE_CONNECTION,
+      useFactory: (configService: ConfigService) => {
+        const pool = new Pool({
+          connectionString: configService.getOrThrow<string>('DATABASE_URL'),
+        });
+        return drizzle(pool);
+      },
+      inject: [ConfigService],
+    },
+  ],
+})
+export class DatabaseModule {}
